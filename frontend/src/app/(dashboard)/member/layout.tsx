8b4bf80d8c445df34693ddfa3ab9c320ea734284
farshid <farshid.1999.fm@gmail.com>
2026-08-27 14:36:24 +0330

    asd

---
+++ frontend/src/app/(dashboard)/member/layout.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { tokenService } from "@/lib/auth/tokenService";
+import AdminSidebar from "@/components/Admin/adminSidebar";
+import "@/styles/light.css"
+
+export default function AdminLayout({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!tokenService.isAuthenticated()) {
+      router.replace("/login");
+    }
+  }, [router]);
+
+  return (
+    <div className="theme-light-admin min-h-screen flex" dir="rtl">
+      <main className="flex-1 overflow-auto p-8">
+        {children}
+      </main>
+    </div>
+  );
+}
